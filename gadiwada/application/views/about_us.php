912bdef3ca7f2df9f5c187e6c1d94c13aead32cf
ravindrakamble4u@gmail.com <ravindrakamble4u@gmail.com@a27c69e8-7f2e-14f7-3d80-b997647c5363>
2014-09-21 14:23:18 +0000

    added new pages

---
+++ gadiwada/application/views/about_us.php
@@ -0,0 +1,22 @@
+<?php $this->load->view('include/header');?>
+<div class="content-boxs">
+<div align="center" style="width: 70%;margin-left:15%;">
+<h3 align="center" class="second">About Us</h3>
+<p align="justify">
+	It all  started in Cuttack, Odisha. Where the online cab bookings was not that much favourable nor the people would rely upon. There were many cab providers but they believed in the concept of mobile booking and the greatest drawback was that the booking could only be done a day or two before the journey date. The urge to provide a seamless travel management system, which would not only provide
+Cab booking but also bus booking gave birth to gadivada.com because of its Odia name the website within a very short span of time achieved a huge popularity in Odisha. But due to the online nature of business the Bus owners were very much reluctant to reserve 2 bus ticket in their bus. However the cab part was working smoothly but without a revenue model as travel agents were pretty reluctant to pay a portion, sparing very few. Being dependent on the Travel agency was not only hampering business but it also requires a lot of paper work to be done a regular basis; to maintain a proper Inventory; to have offline contact. Situations and circumstances gave us a new direction where everything could be automated. This travelder.com was build keeping in mind how to automate the entire travel management system, which created a Win-Win situation for every stakeholder.
+
+</p>
+<p align="left">
+	<strong>Mission</strong>: To provide fast, reliable and quality information to the travellers.
+</p>
+<p align="left">
+	<strong>Vision</strong>: To be the platform which will be an one stop solution for travellers.
+</p>
+<p align="left">
+	<strong>Value</strong>:  Bringing innovation in technology to serve world in their travel.
+</p>
+
+</div>
+</div>
+<?php $this->load->view('include/footer');?>
---
+++ gadiwada/application/views/contact_us.php
@@ -0,0 +1,16 @@
+<?php $this->load->view('include/header');?>
+<div class="content-boxs">
+<div align="center" style="width: 70%;margin-left:15%;">
+<h3 align="center" class="last">Contact Us</h3>
+<p align="justify">
+<strong>
+M-6, Mahatab Nagar,<br />
+Tulsipur, Cuttack, Odisha<br />
+India-753008.<br />
+
+Phone number- 0671-2364673.
+</strong>
+</p>
+</div>
+</div>
+<?php $this->load->view('include/footer');?>
